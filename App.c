19d8424dc252a480a2e37623423168c9a4e9e9a6
“yassinjawad” <alabdulazizy@chc.edu>
2024-02-08 10:44:54 -0500

    2/8/2024

--- App.c
+++ App.c
@@ -1,5 +1,6 @@
 // pre-processor directives
 
 // include needed C and header files
+#include "Assignment.c"
 
 // write code for main function
--- Assignment.c
+++ Assignment.c
@@ -1,5 +1,17 @@
 // pre-processor directives
 
 // include needed header file
+#include "./headers/Assignment.h"
 
 // write code for functions declared in header file
+
+void heartRate (int age, int restingPulse){
+    int i = START;
+
+}
+
+void heartRateReference(int *, int *){
+   
+
+}
+
